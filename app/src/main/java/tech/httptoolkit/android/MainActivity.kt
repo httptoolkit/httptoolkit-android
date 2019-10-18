6aaf10ad075cb6ebf67bbf1f7f749b3bfaf1d251
Tim Perry <pimterry@gmail.com>
2019-10-18 18:39:24 +0200

    Add a toggle button

--- app/src/main/java/tech/httptoolkit/android/MainActivity.kt
+++ app/src/main/java/tech/httptoolkit/android/MainActivity.kt
@@ -2,11 +2,23 @@ package tech.httptoolkit.android
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+
+    private var vpnEnabled: Boolean = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    fun toggleVPN(view: View) {
+        this.vpnEnabled = !this.vpnEnabled
+        val toggleButton = findViewById<TextView>(R.id.toggleButton)
+        toggleButton.text = if (this.vpnEnabled) "Stop Intercepting" else "Start Intercepting"
+    }
+
 }
