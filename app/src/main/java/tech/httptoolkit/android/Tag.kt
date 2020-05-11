6bedb26615d3ee97f2fd9b0c9aa4c531737adb7a
Tim Perry <pimterry@gmail.com>
2020-05-11 16:20:45 +0200

    Handle log tag namespacing in formatTag

--- app/src/main/java/tech/httptoolkit/android/Tag.kt
+++ app/src/main/java/tech/httptoolkit/android/Tag.kt
@@ -4,21 +4,28 @@ import android.os.Build
 
 fun formatTag(tag: String): String {
     return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
-        tag
+        if (tag.startsWith("tech.httptoolkit.android")) {
+            tag
+        } else {
+            // In some cases (e.g. coroutines) our code runs in the context of
+            // other classes - make sure we prefix it for findability regardless.
+            "tech.httptoolkit.android ($tag)"
+        }
     } else {
-        tag.substring(0, 23) // Before API 24 there's a 23 char length limit
+        // Before API 24 there's a 23 char length limit we need to stay under:
+        (
+            if (tag.startsWith("tech.httptoolkit.android")) {
+                tag.replace("tech.httptoolkit.android", "tech.httptoolkit...")
+            } else {
+                // In some cases (e.g. coroutines) our code runs in the context of
+                // other classes - make sure we prefix it for findability regardless.
+                "tech.httptoolkit... ($tag)"
+            }
+        ).substring(0, 23)
     }
 }
 
 val Any.TAG: String
     get() {
-        val name = if (javaClass.name.startsWith("tech.httptoolkit.android")) {
-            javaClass.name
-        } else {
-            // In some cases (e.g. coroutines) our code runs in the context of
-            // other classes - make sure we prefix it for findability regardless.
-            "tech.httptoolkit.android ($javaClass.name)"
-        }
-
-        return formatTag(name)
+        return formatTag(javaClass.name)
     }
