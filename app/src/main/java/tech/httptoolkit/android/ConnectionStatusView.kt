03f10f2eff28f30d8cdbfb9fe86a075891714172
Tim Perry <pimterry@gmail.com>
2020-09-28 15:42:56 +0200

    Handle LineageOS bug that breaks per-app filtering

--- app/src/main/java/tech/httptoolkit/android/ConnectionStatusView.kt
+++ app/src/main/java/tech/httptoolkit/android/ConnectionStatusView.kt
@@ -1,11 +1,17 @@
 package tech.httptoolkit.android
 
 import android.content.Context
+import android.content.Intent
+import android.net.Uri
+import android.os.Build
 import android.text.method.LinkMovementMethod
 import android.view.LayoutInflater
 import android.widget.LinearLayout
 import android.widget.TextView
 import com.google.android.material.card.MaterialCardView
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
+
+private val isLineageOs = Build.HOST.startsWith("lineage")
 
 class ConnectionStatusView(
     context: Context,
@@ -37,7 +43,30 @@ class ConnectionStatusView(
         )
 
         val appInterceptionStatus = findViewById<MaterialCardView>(R.id.appInterceptionStatus)
-        appInterceptionStatus.setOnClickListener { _ -> changeApps() }
+        appInterceptionStatus.setOnClickListener { _ ->
+            if (!isLineageOs) {
+                changeApps()
+            } else {
+                MaterialAlertDialogBuilder(context)
+                    .setTitle("Not available")
+                    .setIcon(R.drawable.ic_exclamation_triangle)
+                    .setMessage(
+                        """
+                        Per-app filtering is not possible on LineageOS, due to a bug in Lineage's VPN implementation.
+
+                        If you'd like this fixed, please upvote the bug in their issue tracker.
+                        """.trimIndent()
+                    )
+                    .setNegativeButton("Cancel") { _, _ -> }
+                    .setPositiveButton("View the bug") { _, _ ->
+                        context.startActivity(Intent(
+                            Intent.ACTION_VIEW,
+                            Uri.parse("https://gitlab.com/LineageOS/issues/android/-/issues/1706")
+                        ))
+                    }
+                    .show()
+            }
+        }
 
         val appInterceptionStatusText = findViewById<TextView>(R.id.appInterceptionStatusText)
         appInterceptionStatusText.text = context.getString(
