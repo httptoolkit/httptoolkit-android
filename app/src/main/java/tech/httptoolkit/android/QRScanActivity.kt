2c206b419c0a5ce6bd854144148a253ff9808a8a
Tim Perry <pimterry@gmail.com>
2025-02-03 19:13:44 +0100

    Remove QR code scan beep

--- app/src/main/java/tech/httptoolkit/android/QRScanActivity.kt
+++ app/src/main/java/tech/httptoolkit/android/QRScanActivity.kt
@@ -7,7 +7,6 @@ import android.util.Log
 import android.view.KeyEvent
 import com.google.zxing.BarcodeFormat
 import com.google.zxing.ResultPoint
-import com.google.zxing.client.android.BeepManager
 import com.journeyapps.barcodescanner.BarcodeCallback
 import com.journeyapps.barcodescanner.BarcodeResult
 import com.journeyapps.barcodescanner.DecoratedBarcodeView
@@ -15,7 +14,6 @@ import com.journeyapps.barcodescanner.DefaultDecoderFactory
 
 class QRScanActivity : Activity() {
     private var barcodeView: DecoratedBarcodeView? = null
-    private var beepManager: BeepManager? = null
     private var lastText: String? = null
 
     private val callback: BarcodeCallback = object : BarcodeCallback {
@@ -30,7 +28,6 @@ class QRScanActivity : Activity() {
             Log.i("QRScanActivity", "Scanned: $resultText")
 
             if (lastText!!.startsWith("https://android.httptoolkit.tech/connect/")) {
-                beepManager!!.playBeepSoundAndVibrate()
                 setResult(RESULT_OK, Intent().putExtra(IntentExtras.SCANNED_URL_EXTRA, lastText))
                 finish()
             }
@@ -48,7 +45,6 @@ class QRScanActivity : Activity() {
         barcodeView!!.initializeFromIntent(intent)
         barcodeView!!.decodeContinuous(callback)
         barcodeView!!.setStatusText("Scan HTTPToolkit QR code to connect")
-        beepManager = BeepManager(this)
     }
 
     override fun onResume() {
